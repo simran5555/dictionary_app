d9c7e031e9f72274dad8b4793d40fc05da3ade86
simran5555 <150110291+simran5555@users.noreply.github.com>
2024-03-20 12:00:03 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+const searchWord = document.getElementById('search')
+const meaning = document.getElementById('meaning')
+const type = document.getElementById('type')
+const phonetics = document.getElementById('phonetics')
+const pronunciation = document.getElementById('pronunciation')
+const word = document.getElementById('word')
+const button = document.querySelector('button')
+
+async function getData() {
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord.value}`
+    console.log(searchWord.value)
+    const data = await fetch(url)
+    const dataJson = await data.json();
+    console.log(dataJson[0])
+
+    pronunciation.style.display = 'block'
+
+    word.innerHTML = dataJson[0].word
+    type.innerHTML = `Part  of Speech: ${dataJson[0]?.meanings[0]?.partOfSpeech}`
+    phonetics.innerHTML = dataJson[0].phonetics[1].text
+    dataJson[0].phonetics[0].audio? pronunciation.src = dataJson[0].phonetics[0].audio : pronunciation.style.display ='none'
+    meaning.innerHTML = `Meaning: ${dataJson[0]?.meanings[0]?.definitions[0]?.definition}`
+
+    searchWord.value =''
+}
+
+
+button.addEventListener('click', getData)
